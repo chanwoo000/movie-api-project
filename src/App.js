779a5e8f9cb4014544aa42e28e276a5369d35916
chanwoo <chanwoo0607@naver.com>
2024-04-04 06:57:16 +0900

    api 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { dummy } from "./movieDummy";
+import Movie from "./components/Movie.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {dummy.results.map((item)=>{
+        return(
+          <Movie
+            title={item.title}
+            poster_path={item.poster_path}
+            vote_average={item.vote_average}
+          />
+        )
+      })}
     </div>
   );
 }
-
 export default App;
